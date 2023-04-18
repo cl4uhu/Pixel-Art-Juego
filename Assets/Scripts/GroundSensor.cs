3c0ed73be45dfe0e39502821acd740077f36fe2d
cl4uhu <pjclaudia1@gmail.com>
2023-04-19 00:13:01 +0200

    update 2

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GroundSensor : MonoBehaviour
 {
+    private PlayerController controller;
+    public bool isGrounded;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+       controller = GetComponentInParent<PlayerController>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true; 
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+        isGrounded = true; 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+        isGrounded = false;
+        }
     }
 }
